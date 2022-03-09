2010ea4be8ff9a507cfc9bc2736bc196bb8206bc
Gaanii <gaeun1884@naver.com>
2022-03-09 14:39:41 +0900

    greedy

--- BOJ/그리디 알고리즘/1931_회의실 배정.py
+++ BOJ/그리디 알고리즘/1931_회의실 배정.py
@@ -7,3 +7,15 @@ for _ in range(N):
     s, f = map(int, sys.stdin.readline().split())
     conf.append([s, f])
 
+conf.sort(key = lambda x : x[0])
+conf.sort(key = lambda x : x[1])
+
+cnt = 0
+finish_time = 0
+
+for i, j in conf:
+    if i >= finish_time:
+        cnt += 1
+        finish_time = j
+
+print(cnt)
