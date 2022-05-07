5ad27fb524c4019a821873ac51c48a2825b295be
Gaanii <gaeun1884@naver.com>
2022-05-07 23:51:16 +0900

    add 이분 탐색 - gold 2 어려워요

---
+++ BOJ/이분 탐색/1300_K번째 수.py
@@ -0,0 +1,18 @@
+# 왜 어려워 이거 다시 풀라면 못풀것같은데 
+n, k = int(input()), int(input())
+start, end = 1, k
+
+while start <= end:
+    mid = (start + end) // 2
+    tmp = 0
+
+    for i in range(1, n+1):
+        tmp += min(mid//i, n)
+
+    if tmp >= k:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)
