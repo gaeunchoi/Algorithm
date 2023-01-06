acacc20dcd46833386f7538e12fea365940fc8a4
Gaanii <gaeun1884@naver.com>
2023-01-06 22:52:11 +0900

    modify 성적평가 시간초과(해결x)

--- softeer/성적 평가.py
+++ softeer/성적 평가.py
@@ -1,23 +1,33 @@
+# 시간초과 해결 x
 import sys
 
 n = int(sys.stdin.readline())
 
-total_score = [0 for _ in range(n)]
+scores = [list(map(int, sys.stdin.readline().split())) for _ in range(3)]
+total_score = [x+y+z for x, y, z in zip(scores[0], scores[1], scores[2])]
 
-for _ in range(3):
-    scores = list(map(int, sys.stdin.readline().split()))
-    rank = [0 for _ in range(n)]
+for score in scores:
+    ranks = ''
+    for i in range(n):
+        cnt = len(list(filter(lambda num: num > score[i], score)))
+        ranks += (str(cnt+1) + " ")
+        total_score[i] += score[i]
+    print(ranks)
 
-    for i in range(len(scores)):
-        # cnt = 0
-        cnt = len(list(filter(lambda  num: num > scores[i], scores)))
-        rank[i] = cnt+1
-        total_score[i] += scores[i]
-        print(rank[i], end=" ")
-    print()
+for score in total_score:
+    print(len(list(filter(lambda num: num > score, total_score)))+1, end=' ')
 
-for i in range(len(total_score)):
-    cnt = len(list(filter(lambda num: num > total_score[i], total_score)))
-    print(cnt+1, end = " ")
+# for _ in range(3):
+#     scores = list(map(int, sys.stdin.readline().split()))
+#     rank = ""
+#
+#     for i in range(len(scores)):
+#         cnt = len(list(filter(lambda num: num > scores[i], scores)))
+#         rank += (str(cnt+1) + " ")
+#         total_score[i] += scores[i]
+#     print(rank)
+#
+# for i in range(len(total_score)):
+#     cnt = len(list(filter(lambda num: num > total_score[i], total_score)))
+#     print(cnt+1, end = " ")
 
-# 시간초과 뚫기
