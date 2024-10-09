e9eedacee4b2ec3f54caf2e9fdfbf537399ce942
Gaanii <gaeun1884@naver.com>
2024-10-09 23:05:19 +0900

    commit 241009 BOJ

---
+++ BOJ/1차원 배열/10811_바구니 뒤집기.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+basket = [i+1 for i in range(n)]
+
+for x in range(m):
+    i, j = map(int, input().split())
+    reversed_basket = list(reversed(basket[i-1: j]))
+    basket[i-1: j] = reversed_basket
+
+for ele in basket:
+    print(ele, end = " ")
+# 21345
+# 21435
+# 34125
+# 34125
---
+++ BOJ/심화 1/10988_팰린드롬인지 확인하기.py
@@ -0,0 +1,24 @@
+input_word = input()
+palindrome = 0
+
+pivot = len(input_word) // 2
+if len(input_word) % 2 == 0:
+
+    front = input_word[:pivot]
+    back = ''.join(reversed(input_word[pivot:]))
+    if front == back:
+        palindrome = 1
+else:
+    front = input_word[:pivot]
+    back = ''.join(reversed(input_word[pivot+1:]))
+    if front == back:
+        palindrome = 1
+
+print(palindrome)
+
+
+
+if ''.join(reversed(input_word)) == input_word:
+    print(1)
+else:
+    print(0)
