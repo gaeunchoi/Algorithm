ede334b1afe267f10ba9e738941ba757e82639d1
Gaanii <gaeun1884@naver.com>
2024-10-17 21:36:38 +0900

    commit 241017 프로그래머스 | Lv1

---
+++ 프로그래머스/241017/달리기 경주.py
@@ -0,0 +1,14 @@
+def solution(players, callings):
+    answer = []
+    ranks = {name: rank for rank, name in enumerate(players)}
+    for call in callings:
+        idx = ranks[call]
+
+        ranks[call] -= 1
+        ranks[players[idx-1]] += 1
+
+        players[idx-1], players[idx] = players[idx], players[idx-1]
+
+    return players
+
+print(solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"]))
