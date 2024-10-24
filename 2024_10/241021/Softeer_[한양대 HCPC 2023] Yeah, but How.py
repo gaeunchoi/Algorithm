65e0e0d678ec4b11bb581483a7d88d6d0dad6546
Gaanii <gaeun1884@naver.com>
2024-10-24 22:20:07 +0900

    commit 241024 Softeer | 1021 못푼 문제 해결

--- 2024_10/241021/Softeer_[한양대 HCPC 2023] Yeah, but How.py
+++ 2024_10/241021/Softeer_[한양대 HCPC 2023] Yeah, but How.py
@@ -4,35 +4,35 @@ input_str = sys.stdin.readline().rstrip()
 print(input_str.replace('()', '(1)').replace(')(', ')+('))
 
 # ------------------------------------------------------------------------------------
-from collections import deque
-import sys
-
-input_str = deque(sys.stdin.readline())
-result = deque()
-
-isFirst = True
-
-left_sym = input_str.popleft()
-current_sym = input_str.popleft()
-
-while left_sym != '\n':
-    result.append(left_sym)
-
-    if isFirst == True:
-        isFirst = False
-    else:
-        current_sym = input_str.popleft()
-
-    if left_sym == '(' and current_sym == '(':
-        result.append('1+')
-    if left_sym == '(' and current_sym == ')':
-        result.append('1')
-    if left_sym == ')' and current_sym == '(':
-        result.append('+')
-    if left_sym == ')' and current_sym == ')':
-        result.append('+1')
-
-    left_sym = current_sym
-
-for i in range(len(result)):
-    print(result[i], end='')
+# from collections import deque
+# import sys
+#
+# input_str = deque(sys.stdin.readline())
+# result = deque()
+#
+# isFirst = True
+#
+# left_sym = input_str.popleft()
+# current_sym = input_str.popleft()
+#
+# while left_sym != '\n':
+#     result.append(left_sym)
+#
+#     if isFirst == True:
+#         isFirst = False
+#     else:
+#         current_sym = input_str.popleft()
+#
+#     if left_sym == '(' and current_sym == '(':
+#         result.append('1+')
+#     if left_sym == '(' and current_sym == ')':
+#         result.append('1')
+#     if left_sym == ')' and current_sym == '(':
+#         result.append('+')
+#     if left_sym == ')' and current_sym == ')':
+#         result.append('+1')
+#
+#     left_sym = current_sym
+#
+# for i in range(len(result)):
+#     print(result[i], end='')
