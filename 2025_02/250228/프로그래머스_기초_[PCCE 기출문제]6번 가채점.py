28d6621b2fdb7ca74098c2058aef78cc52783fe5
gaeunchoi <gaeun1884@naver.com>
2025-02-28 17:42:33 +0900

    Docs: 250228 프로그래머스 기초 트레이닝 - Python

---
+++ 2025_02/250228/프로그래머스_기초_[PCCE 기출문제]6번 가채점.py
@@ -0,0 +1,13 @@
+def solution(numbers, our_score, score_list):
+    answer = []
+    for i in range(len(numbers)):
+        print(our_score[i], score_list[numbers[i]-1])
+        if our_score[i] == score_list[numbers[i]-1]:
+            answer.append("Same")
+        else:
+            answer.append("Different")
+    
+    return answer
+
+print(solution([1], [100], [100, 80, 90, 84, 20]))
+print(solution([3, 4], [85, 93], [85, 92, 38, 93, 48, 85, 92, 56]))
