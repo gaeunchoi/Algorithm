8a50c6dc7c934f6adf3732e9380385bb7c643811
gaeunchoi <gaeun1884@naver.com>
2025-03-10 11:46:11 +0900

    Rename: 2025-03-10 README.md 추가 및 파일 재분류

--- 2025_03/[프로그래머스 | 알고리즘 고득점Kit] 기능 개발/기능개발.py
+++ 2025_03/[프로그래머스 | 알고리즘 고득점Kit] 기능 개발/기능개발.py
@@ -3,19 +3,19 @@ from math import ceil
 def solution(progresses, speeds):
     answer = []
     days = []
-    pivot = 0
+    front = 0
 
     # 소요시간을 정리해두자
     for i in range(len(progresses)):
         days.append(ceil((100 - progresses[i]) / speeds[i]))
-
+    
     for idx in range(len(days)):
-        if days[idx] > days[pivot]:
-            answer.append(idx - pivot)
-            pivot = idx
-    answer.append(len(days) - pivot)
+        if days[idx] > days[front]:
+            answer.append(idx - front)
+            front = idx
+    answer.append(len(days) - front)
 
     return answer
 
-print(solution([93, 30, 55], [1, 30, 5]))
-print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
+print(solution([93, 30, 55],[1, 30, 5]))
+print(solution([95, 90, 99, 99, 80, 99],[1, 1, 1, 1, 1, 1]))
