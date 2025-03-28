f777df8d5a9f948a6df14d18dc639af82f14109b
gaeunchoi <gaeun1884@naver.com>
2025-03-28 14:16:20 +0900

    Test: 2025-03-28 DEEPDIVE 알고리즘 미션 | 여유 황금비

---
+++ 2025_03/DEEPDIVE 알고리즘 미션/기초 수학/여유 황금비/여유 황금비.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+T = int(input())
+
+result = 0
+for _ in range(T):
+	A, B = map(int, input().split())
+	small, big = (A, B) if A < B else (B, A)
+	if 1.6 * small <= big <= 1.63 * small:
+		result += 1
+		
+print(result)
